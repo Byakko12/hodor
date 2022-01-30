addeb8323f61517ee7b23c587a9520167e8f923d
David <davidlandg@gmail.com>
2022-01-30 14:27:31 -0500

    [FIX] - id

--- level_0/level_0.py
+++ level_0/level_0.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import requests
 
-data_form = {'id':'3892','holdthedoor':'submit'}
+data_form = {'id':'3489','holdthedoor':'submit'}
 
 for i in range(1024):
     requests.post("http://158.69.76.135/level0.php", data=data_form)
