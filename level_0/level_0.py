ff4351d2c97ea51eb2cee24d9ff7d4d262ef217a
David <davidlandg@gmail.com>
2022-01-30 14:03:06 -0500

    [ADD] - level0

---
+++ level_0/level_0.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import requests
+
+data_form = {'id':'3892','holdthedoor':'submit'}
+
+for i in range(1024):
+    requests.post("http://158.69.76.135/level0.php", data=data_form)
+
+    print("vote:{}".format(i))
